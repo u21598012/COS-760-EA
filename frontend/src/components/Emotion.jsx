b98b3fb9a3b0aa05963b7fb9449555c99128d57d
Yudi-G <133056840+Yudi-G@users.noreply.github.com>
2025-06-15 17:35:39 +0200

    adding colors

--- frontend/src/components/Emotion.jsx
+++ frontend/src/components/Emotion.jsx
@@ -1,21 +1,65 @@
 import { useState } from "react";
 import axios from "axios";
 
+import Highlighter from "react-highlight-words";
+
 function Emotion() {
     const [input, setInput] = useState("");
     const [result, setResult] = useState(null);
     const [expl, setExpl] = useState(null);
-
+    const [words1, setWords1] = useState(null);
+    const [words2, setWords2] = useState(null);
+    const colours = []
     const handleSubmit = async () => {
         try {
             const res = await axios.get("http://localhost:8000/");
             setResult(res.data);
             // setInput(res.data.text)
+            const topEmotion = Object.entries(res.data.predictions)
+                .sort((a, b) => b[1] - a[1])[0][0];
+            
+            const threshold = 0.1;
+            const filtered = {};
+            for (const [emotion, wordPairs] of Object.entries(res.data.explanations)) {
+                if(emotion===topEmotion)
+                    filtered[emotion] = wordPairs.filter(([word, score]) => (score >= threshold));
+            }
+
+            const topEmotion2 = Object.entries(res.data.predictions)
+                .sort((a, b) => b[1] - a[1])[1][0];
+            console.log(topEmotion2)
+            const filtered2 = {};
+
+            for (const [emotion, wordPairs] of Object.entries(res.data.explanations)) {
+                if(emotion===topEmotion2)
+                    filtered2[emotion] = wordPairs.filter(([word, score]) => (score >= threshold));
+            }
+            const mergedFiltered = { ...filtered, ...filtered2 };
+
+            const words1 = Object.values(filtered).flat().map(([word]) => word);
+            const words2 = Object.values(filtered2).flat().map(([word]) => word);
+            setWords1(words1);
+            setWords2(words2);
+
+            console.log(words1);
+            console.log(mergedFiltered);
+            setExpl(filtered);
         }
         catch(err) {
             console.error(err);
             alert("api fail");
         }
+    }
+
+    function getImportantWords(data, predictionWord, threshold) {
+        const explanationPairs = data.explanations[predictionWord];
+        if (!explanationPairs) {
+            return [];
+        }
+
+        return explanationPairs
+            .filter(([word, score]) => Math.abs(score) >= threshold)
+            .map(([word]) => word);
     }
 
     return (
@@ -33,17 +77,29 @@ function Emotion() {
             </button>
 
             {result && (
+                
                 <div className="result-div"> 
+
                     <h3>Predicted Emotions</h3>
                     <table className="table">
                         <tr>
                             <th>Emotion</th>
                             <th>Value</th>
+                            <th>Words</th>
                         </tr>
                         {Object.entries(result.predictions).map(([emotion, val], idx) => (
                             <tr key={idx}>
                                 <td>{emotion}</td>
                                 <td>{val}</td>
+                                <td>
+                                    <Highlighter
+                                        highlightClassName="highlight"
+                                        searchWords={getImportantWords(result, emotion, 0.1)}
+                                        autoEscape={true}
+                                        textToHighlight={result.text}
+                                        highlightStyle={{ backgroundColor: "#12f54f" }}
+                                    />
+                                </td>
                             </tr>
                         ))}
                     </table>
