5d540bb083170c5a359cbfd5a6aea789ebc5d91d
Yudi-G <133056840+Yudi-G@users.noreply.github.com>
2025-06-15 14:11:38 +0200

    added more to emotion

--- frontend/src/components/Emotion.jsx
+++ frontend/src/components/Emotion.jsx
@@ -2,26 +2,53 @@ import { useState } from "react";
 import axios from "axios";
 
 function Emotion() {
-    const [inputText, setInputText] = useState("");
+    const [input, setInput] = useState("");
+    const [result, setResult] = useState(null);
+    const [expl, setExpl] = useState(null);
 
-    const handleSubmit = () => {
-        alert("submitted")
+    const handleSubmit = async () => {
+        try {
+            const res = await axios.get("http://localhost:8000/");
+            setResult(res.data);
+            // setInput(res.data.text)
+        }
+        catch(err) {
+            console.error(err);
+            alert("api fail");
+        }
     }
 
     return (
         <div className="outer">
-        <h2 className="title">NLP Emotion Analyzer</h2>
+            <h2 className="title">NLP Emotion Analyzer</h2>
 
-        <textarea
-            value={inputText}
-            onChange={(e) => setInputText(e.target.value)}
-            placeholder="Enter text here"
-            className="textarea"
-        />
-        <button onClick={handleSubmit} className="buttonsubmit">
-            Analyze
-        </button>
+            <textarea
+                value={input}
+                onChange={(e) => setInput(e.target.value)}
+                placeholder="Enter text here"
+                className="textarea"
+            />
+            <button onClick={handleSubmit} className="buttonsubmit">
+                Analyze
+            </button>
 
+            {result && (
+                <div className="result-div"> 
+                    <h3>Predicted Emotions</h3>
+                    <table className="table">
+                        <tr>
+                            <th>Emotion</th>
+                            <th>Value</th>
+                        </tr>
+                        {Object.entries(result.predictions).map(([emotion, val], idx) => (
+                            <tr key={idx}>
+                                <td>{emotion}</td>
+                                <td>{val}</td>
+                            </tr>
+                        ))}
+                    </table>
+                </div>
+            )}
         </div>
     );
 }
